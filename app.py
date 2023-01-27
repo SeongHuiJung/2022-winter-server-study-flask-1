5c56bfe9740dcdf3397175f38a4556f58a924b0b
SeongHuiJung <tjdgml150237@naver.com>
2023-01-27 16:44:02 +0900

    과제

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+    if id >= 5000:
+        return 'true'
+    else:
+        return 'false'
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    data={'name': '야호', 'value': '52'}
+    return data['name']
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    data={'name': '야호','value': '52'}
+    return data['name']
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
