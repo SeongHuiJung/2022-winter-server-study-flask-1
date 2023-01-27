7608455535e121c106ed5d0a1245f58d6c999a28
SeongHuiJung <tjdgml150237@naver.com>
2023-01-27 18:44:49 +0900

    과제 수정

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
+
     if id >= 5000:
-        return 'true'
+        data={'result':'true'}
     else:
-        return 'false'
+        data={'result':'false'}
+    return data['result']
 
 @app.route('/id', methods=['POST'])
 def post():
-    data={'name': '야호', 'value': '52'}
-    return data['name']
+    data={'name': 'hello', 'value': '52'}
+    return jsonify(data['name'])
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    data={'name': '야호','value': '52'}
-    return data['name']
+    data={'name': 'hello','value': '52'}
+    return jsonify(data['name'])
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
